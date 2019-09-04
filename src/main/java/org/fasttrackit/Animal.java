d84c109a3bc2065a0c3418ce4b556ff64c5ced0c
Pop Adriana <anni.ania19@yahoo.com>
2019-09-04 12:39:27 +0300

    basic project structure

---
+++ src/main/java/org/fasttrackit/Animal.java
@@ -0,0 +1,35 @@
+package org.fasttrackit;
+
+import java.lang.reflect.Type;
+
+public class Animal {
+
+   String type;
+   double needForSleep;
+   double hungerLevel;
+   int needForGames;
+   boolean needMedicalServices;
+
+   public double getNeedForSleep (double DurationInHours) {
+            return needForSleep;
+   }
+
+   public double setNeedForGames( double time ) {
+           return time;
+   }
+
+   public void setNeedMedicalServices(boolean needMedicalServices ) {
+      this.needMedicalServices = needMedicalServices;
+   }
+
+   public void setHungerLevel (double food, double meat, double milk, double fruits )
+
+
+
+
+   {
+
+   }
+
+
+}
--- src/main/java/org/fasttrackit/App.java
+++ src/main/java/org/fasttrackit/App.java
@@ -4,10 +4,20 @@ package org.fasttrackit;
  * Hello world!
  *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
     }
+
+    {
+        Animal animal = new Animal();
+        animal.type = "Bobcat";
+        animal.hungerLevel = 5;
+        animal.needMedicalServices = true;
+        animal.needForGames = 12;
+        animal.needForSleep = 13;
+
+
+    }
+
 }
