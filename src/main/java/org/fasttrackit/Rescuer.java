163f232a37ec242fb49085d7a66dd0c2566d7313
Pop Adriana <anni.ania19@yahoo.com>
2019-09-06 13:15:24 +0300

    added methods in Rescuer Class

--- src/main/java/org/fasttrackit/Rescuer.java
+++ src/main/java/org/fasttrackit/Rescuer.java
@@ -1,14 +1,19 @@
 package org.fasttrackit;
 
+
+
 public class Rescuer {
 
     String name;
     double coins;
 
-    public  void  feedTheAnimal (String yiti, double hungerLevel ) {
+    public  void  feedTheAnimal (String animalName, double hungerLevel, double meat ) {
 
-        System.out.println( name + "just gave some" + "meat" + name + "result" + hungerLevel + "decrease" );
+        System.out.println( animalName + "just gave some" + meat + animalName + "result" + hungerLevel + "decrease" ); }
 
+
+    public void caressedTheAnimal ( String animalName, double needToBeCaressed){
+        System.out.println( animalName + "just gave some joy activity" + "is now purring" + "and feel much happier" );
     }
 
 
